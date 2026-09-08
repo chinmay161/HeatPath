52dd51bdb7eb83571303201ce1c5d2403fa895d5
chinmay <chinmaywank12@gmail.com>
2026-09-08 19:55:51 +0530

    fix(mobile): configure native maps for Android and iOS

---
+++ mobile/app.config.js
@@ -0,0 +1,19 @@
+module.exports = ({ config }) => {
+  const googleMapsApiKey =
+    process.env.GOOGLE_MAPS_API_KEY ||
+    process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ||
+    '';
+
+  return {
+    ...config,
+    android: {
+      ...config.android,
+      config: {
+        ...config.android?.config,
+        googleMaps: {
+          apiKey: googleMapsApiKey,
+        },
+      },
+    },
+  };
+};
